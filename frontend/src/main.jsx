dbf91338309ba016727dc6107a8ae42781ba37d3
byamukama211 <abubakarsaad211@gmail.com>
2025-11-14 12:58:37 +0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,10 +5,6 @@ import App from './App';
 import './styles.css';
 import ReactDOM from 'react-dom/client'
 
-
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
