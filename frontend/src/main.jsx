8c9324ac25cb02ef1bfd6dc1d564a3bd0f5a761c
byamukama211 <abubakarsaad211@gmail.com>
2025-11-14 12:56:41 +0300

    Wrap App component with BrowserRouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,9 +5,13 @@ import App from './App';
 import './styles.css';
 import ReactDOM from 'react-dom/client'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
-)
+  </BrowserRouter>
+);
 
